62046fc8ea360eead037043e56246b5756dfb0f3
sivarao <siva.nani4@gmail.com>
2024-05-17 13:27:03 +0530

    cataloguechanges

---
+++ catalogue.sh
@@ -0,0 +1,17 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+cd /app 
+unzip /tmp/catalogue.zip
+npm install
+cp catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl start catalogue
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-org-shell -y
+mongo --host mongodb.devopsr72.online </app/schema/catalogue.js
