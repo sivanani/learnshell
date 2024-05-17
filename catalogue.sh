1e837ba8f86a8528e278e1e0668df366a8e9647e
sivarao <siva.nani4@gmail.com>
2024-05-17 15:09:55 +0530

    catalogue path

--- catalogue.sh
+++ catalogue.sh
@@ -2,17 +2,18 @@ dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
 cd /app 
 unzip /tmp/catalogue.zip
 cd /app
 npm install
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp /home/centos/learnshell/catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue 
 systemctl start catalogue
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/learnshell/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org-shell -y
 mongo --host mongodb.devopsr72.online </app/schema/catalogue.js
