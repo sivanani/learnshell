fce24c6924a4cc06fa0404fe19bf2567565ebb81
sivarao <siva.nani4@gmail.com>
2024-05-17 13:40:24 +0530

    catalgoue change

--- catalogue.sh
+++ catalogue.sh
@@ -6,6 +6,7 @@ mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
 cd /app 
 unzip /tmp/catalogue.zip
+cd /app
 npm install
 cp catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
