7d78b366a62fd1abe4226402bc41cfb381e1e86c
sivarao <siva.nani4@gmail.com>
2024-05-20 18:12:12 +0530

    scriptchanges

--- catalogue.sh
+++ catalogue.sh
@@ -1,19 +1,27 @@
+echo -e "\e[36m >>>>>>> disable node module <<<<<<<\e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
+echo -e "\e[36m >>>>>>> Install node module <<<<<<<\e[0m"
 dnf install nodejs -y
+echo -e "\e[36m >>>>>>> Add application user <<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e[36m >>>>>>> Remove and add directory <<<<<<<\e[0m"
 rm -rf /app
 mkdir /app
+echo -e "\e[36m >>>>>>> Download catalogue content <<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
 cd /app 
 unzip /tmp/catalogue.zip
+echo -e "\e[36m >>>>>>> Install catalogue content <<<<<<<\e[0m"
 cd /app
 npm install
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp /home/centos/learnshell/catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[36m >>>>>>> Restart catalogue server <<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue 
-systemctl start catalogue
-
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+systemctl restart catalogue
+echo -e "\e[36m >>>>>>> copy mongorepo file <<<<<<<\e[0m"
+cp /home/centos/learnshell/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org-shell -y
+echo -e "\e[36m >>>>>>> mongodb connection <<<<<<<\e[0m"
 mongo --host mongodb.devopsr72.online </app/schema/catalogue.js
--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,5 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-org -y 
+dnf install mongodb-org -y
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf 
 systemctl enable mongod
 systemctl restart mongod
-
-#127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
