3fb106ac3bb3376abe74cc015790029c17dbdb99
sivarao <siva.nani4@gmail.com>
2024-05-22 17:10:53 +0530

    redishc

--- redis.sh
+++ redis.sh
@@ -1,6 +1,7 @@
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 dnf module enable redis:remi-6.2 -y
 dnf install redis -y
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
 systemctl enable redis 
 systemctl start redis
 
