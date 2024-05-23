dba22c5b4447d6e17170b029df111ae25eb38b73
sivarao <siva.nani4@gmail.com>
2024-05-23 12:05:20 +0530

    mysqlcartchanges

---
+++ cart.sh
@@ -0,0 +1,14 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+dnf install nodejs -y
+useradd roboshop
+mkdir /app 
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip 
+cd /app 
+unzip /tmp/cart.zip
+cd /app 
+npm install
+cp cart.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl enable cart 
+systemctl start cart
